933c9069f0bd9ba8d020ba0fa93d98a56cdfa9ce
7roubb <7roubb@gmail.com>
2024-07-12 12:20:31 +0300

    1st Day

---
+++ Watermelon.cpp
@@ -0,0 +1,20 @@
+// Date : 12/7/2024     Osama Alhroub
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int wieght;
+    cin >> wieght;
+    if (wieght % 2 == 0 && wieght > 2)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
---
+++ Way Too Long Words.cpp
@@ -0,0 +1,23 @@
+// Date : 12/7/2024     Osama Alhroub
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    string w;
+    cin >> n;
+    while (n--)
+    {
+        cin >> w;
+        if (w.length() > 10)
+        {
+            cout << w[0] << w.length() - 2 << w[w.length() - 1] << endl;
+        }
+        else
+        {
+            cout << w << endl;
+        }
+    }
+    return 0;
+}
